b61df749d3a4063f186456fe952aca793fcf881d
pei <ippei.223miyake119@gmail.com>
2019-08-29 21:57:14 +0900

    不要なファイル削除

--- src/renderer/entity/SearchInfo.ts
+++ src/renderer/entity/SearchInfo.ts
@@ -1,3 +1,5 @@
+import TodoEntity from "./TodoEntity";
+
 /**
  * 検索処理に必要な情報.
  * TODO 扱うデータに合わせる
@@ -21,5 +23,21 @@ export default class SearchInfo {
 
     private isValidDate(date: Date): boolean {
         return date !== null && date.getTime instanceof Function && !isNaN(date.getTime())
+    }
+
+    isApplicableToSearchInfo(todo: TodoEntity): boolean {
+        if (this.keyword !== null && todo.title.indexOf(this.keyword) === -1) {
+            return false
+        }
+        if (this.checked !== null && this.checked !== todo.checked) {
+            return false
+        }
+        if (this.startDate !== null && this.startDate.getTime() > todo.createdAt) {
+            return false
+        }
+        if (this.endDate !== null && this.endDate.getTime() < todo.createdAt) {
+            return false
+        }
+        return true
     }
 }
